8ee65475a7df9cf74baa1547d25b1ed7f4e8548b
Matt Riley <mattrileyaus@gmail.com>
2020-08-21 23:18:19 +1000

    upgrade module-composer and use callback option

--- boot.js
+++ boot.js
@@ -1,43 +1,146 @@
-const merge = require('lodash.merge');
-const composeSrc = require('module-composer')(require('./src'));
+const composer = require('module-composer');
+const src = require('./src');
 
 module.exports = ({ window, ...overrides }) => {
 
-    const compose = (key, arg) => merge(composeSrc(key, arg), overrides[key]);
+    const context = composer(src, { overrides }, compose => {
 
-    const config = compose('config', { window });
-    const io = compose('io', { config, window });
-    const util = compose('util', { config });
-
-    const storage = compose('storage', { config });
-    const { state, stores, settings, subscriptions } = storage.initialise();
-
-    const core = compose('core', { config });
-    const services = compose('services', { subscriptions, settings, stores, core, io, util, config });
+        const config = compose('config', { window });
+        const io = compose('io', { config, window });
+        const util = compose('util', { config });
     
-    const elements = compose('elements', { io, window });
-    const { el } = elements;
-    const components = compose('components', { el, elements, services, subscriptions, util, config, window });
+        const { stores, settings, subscriptions } = compose('storage', { config }, storage => storage.initialise());
     
-    services.system.initialise();
-    
-    const context = {        
-        elements,
-        components,
-        services,
-        storage,
-        state,
-        stores,
-        settings,
-        subscriptions,
-        util,
-        core,
-        io,
-        config,
-        window
-    };
+        const core = compose('core', { config });
+        const services = compose('services', { subscriptions, settings, stores, core, io, util, config });
+        
+        const elements = compose('elements', { io, window });
+        const { el } = elements;
+        compose('components', { el, elements, services, subscriptions, util, config, window });
 
+    });
+
+    context.services.system.initialise();
     window.agileavatars = context;
     return context;
     
 };
+
+
+// const merge = require('lodash.merge');
+// const composer = require('module-composer');
+// const src = require('./src');
+
+// module.exports = ({ window, ...overrides }) => {
+
+//     const context = composer(src, compose1 => {
+
+//         const compose = (key, arg, cb) => {
+//             const res1 = compose1(key, arg, res => {                
+//                 return cb ? cb(res) : res;
+//                 // const a = merge(res, overrides[key]);
+//             });
+//             console.log(key, res1);
+//             return res1;
+//         };
+    
+//         const { config } = compose('config', { window });
+//         const { io } = compose('io', { config, window });
+//         const { util } = compose('util', { config });
+    
+//         const { stores, settings, subscriptions } = compose('storage', { config }, ({ storage }) => storage.initialise());
+    
+//         const { core } = compose('core', { config });
+//         const { services } = compose('services', { subscriptions, settings, stores, core, io, util, config });
+        
+//         const { elements } = compose('elements', { io, window });
+//         const { el } = elements;
+//         compose('components', { el, elements, services, subscriptions, util, config, window });
+        
+
+//     });
+
+    
+//     context.services.system.initialise();
+
+//     console.log(context);
+    
+//     // const context = {        
+//     //     elements,
+//     //     components,
+//     //     services,
+//     //     storage,
+//     //     state,
+//     //     stores,
+//     //     settings,
+//     //     subscriptions,
+//     //     util,
+//     //     core,
+//     //     io,
+//     //     config,
+//     //     window
+//     // };
+
+
+//     window.agileavatars = context;
+//     return context;
+    
+// };
+
+
+// const merge = require('lodash.merge');
+// const composer = require('module-composer');
+// const src = require('./src');
+
+// module.exports = ({ window, ...overrides }) => {
+
+//     const context = composer(src, compose1 => {
+
+//         const compose = (key, arg, cb) => {
+//             const res1 = compose1(key, arg, res => merge(res, overrides[key]));
+//             const res2 = cb ? cb(res1) : { [key]: res1 };
+//             return res2;
+//         };
+    
+//         const { config } = compose('config', { window });
+//         const { io } = compose('io', { config, window });
+//         const { util } = compose('util', { config });
+    
+//         const { stores, settings, subscriptions } = compose('storage', { config }, storage => storage.initialise());
+    
+//         const { core } = compose('core', { config });
+//         const { services } = compose('services', { subscriptions, settings, stores, core, io, util, config });
+        
+//         const { elements } = compose('elements', { io, window });
+//         const { el } = elements;
+//         compose('components', { el, elements, services, subscriptions, util, config, window });
+        
+
+//     });
+
+    
+//     context.services.system.initialise();
+
+//     console.log(context);
+    
+//     // const context = {        
+//     //     elements,
+//     //     components,
+//     //     services,
+//     //     storage,
+//     //     state,
+//     //     stores,
+//     //     settings,
+//     //     subscriptions,
+//     //     util,
+//     //     core,
+//     //     io,
+//     //     config,
+//     //     window
+//     // };
+
+
+//     window.agileavatars = context;
+//     return context;
+    
+// };
