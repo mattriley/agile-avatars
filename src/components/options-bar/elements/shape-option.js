aa481f745a0ba0fbb3031ba813a4e027ff7c3b40
Matt Riley <mattrileyaus@gmail.com>
2020-08-24 21:34:39 +1000

    tabindex for shape option and selection with enter or space key

--- src/components/options-bar/elements/shape-option.js
+++ src/components/options-bar/elements/shape-option.js
@@ -1,9 +1,15 @@
 module.exports = ({ el, subscriptions, services, lib, config }) => shapeName => {
     
     const $shape = el('span', 'shape-option', {
-        title: `Change shape to ${shapeName}`
+        title: `Change shape to ${shapeName}`,
+        tabIndex: 0
     }).addEventListener('click', () => {
         services.settings.changeOption('shape', shapeName);
+    }).addEventListener('keydown', e => {            
+        if (['Enter', 'Space'].includes(e.code)) {
+            $shape.click();
+            e.preventDefault();
+        }
     });
 
     const borderRadius = config.options.shapeRadius[shapeName] ?? 0;
--- test/tests/components/shape.test.js
+++ test/tests/components/shape.test.js
@@ -1,3 +1,5 @@
+// TODO: Explicit tests for toggling with Enter and Space keys.
+
 module.exports = ({ test, boot, window, helpers }) => {
 
     const toggleTestCase = (t, shapeName) => {
@@ -18,7 +20,7 @@ module.exports = ({ test, boot, window, helpers }) => {
         window.document.body.append($styles); 
         const $tagList = components.tagList();
         services.tags.insertTag(); 
-        helpers.dispatchEvent('click', $shape); 
+        helpers.dispatchKeydown($shape, 'Enter'); 
         const [tag1] = helpers.getTags($tagList);
         const imageStyle = tag1.getImageStyle();
         t.equal(imageStyle.borderRadius, `${config.options.shapeRadius[shapeName]}%`);
@@ -39,6 +41,19 @@ module.exports = ({ test, boot, window, helpers }) => {
 
     test('circle shape applied to tag', t => {
         shapeTestCase(t, 'circle');
+    });
+
+    // TODO: Remove duplication
+    test('shape not selected on keypress other than Enter or Space', t => {
+        const shapeName = 'circle';
+        const { components } = boot();
+        const $shape = components.optionsBar.elements.shapeOption(shapeName);   
+        const $anotherShape = components.optionsBar.elements.shapeOption('foobar');
+        helpers.dispatchEvent('click', $anotherShape);
+        helpers.assertBoolClass(t, $shape, 'selected', false);
+        helpers.dispatchKeydown($shape, 'A');
+        helpers.assertBoolClass(t, $shape, 'selected', false);
+        t.end();
     });
 
 };
