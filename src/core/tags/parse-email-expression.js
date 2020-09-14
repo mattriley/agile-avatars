e6505c7dea45757f583d03af1de403a80547f2e8
Matt Riley <mattrileyaus@gmail.com>
2020-09-14 23:16:00 +1000

    refactor

--- src/core/tags/parse-email-expression.js
+++ src/core/tags/parse-email-expression.js
@@ -1,29 +1,18 @@
-module.exports = ({ core, lib }) => expression => {
-
-    const result = data => {
-        const emailOrUsername = data.email ?? data.username;
-        return { emailOrUsername, ...data };
-    };
-
-    const isEmail = expression.includes('@');
-    
-    if (!isEmail) {
-        const { tagName: username, ...data } = core.tags.parseTagExpression(expression);
-        return result({ username, ...data });
-    }
+module.exports = ({ lib }) => expression => {
 
     const indexOfAt = expression.indexOf('@');
-    const [username] = expression.substr(0, indexOfAt).split('+');
+    const [username] = (indexOfAt > -1 ? expression.substr(0, indexOfAt) : expression).split('+');
     const lastIndexOfPlus = expression.lastIndexOf('+');
     const hasRole = lastIndexOfPlus > indexOfAt;        
-    const [email = '', roleName] = hasRole ? lib.util.splitAt(expression, lastIndexOfPlus, 1) : [expression];
-    return result({ username, email, roleName });
+    const [email, roleName] = hasRole ? lib.util.splitAt(expression, lastIndexOfPlus, 1) : [expression];
+    const emailOrUsername = email ?? username;
+    return { email, username, emailOrUsername, roleName };
 
 };
 
 /* FOOTNOTES
 
 Example of complex expression: 'foo+bar@gmail.com+dev'
-=> { tagName: 'foo', roleName: 'dev', email: 'foo+bar@gmail.com' }
+=> { email: 'foo+bar@gmail.com', username: 'foo', emailOrUsername: 'foo+bar@gmail.com', roleName: 'dev' }
 
 */
