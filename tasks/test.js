a1ace1f0037949db97905354d6e1fca0a21663cb
Matt Riley <mattrileyaus@gmail.com>
2020-09-15 21:24:03 +1000

    wrap test setup in function

--- tasks/test.js
+++ tasks/test.js
@@ -9,17 +9,23 @@ const composer = require('module-composer');
 const testHelpers = require('../test-helpers');
 const bootOrig = require('../boot');
 
-const { window } = new JSDOM('', { url: 'https://localhost/' });
-window.fetch = () => undefined;
-const resetJsdom = () => { window.document.getElementsByTagName('html')[0].innerHTML = ''; };
-const helpers = composer({ helpers: testHelpers })('helpers', { window });
+const setup = () => {
+    const { window } = new JSDOM('', { url: 'https://localhost/' });
+    window.fetch = () => undefined;
+    const resetJsdom = () => { window.document.getElementsByTagName('html')[0].innerHTML = ''; };
+    const helpers = composer({ helpers: testHelpers })('helpers', { window });
 
-const boot = (args = {}) => {
-    resetJsdom(); 
-    const defaultConfig = { debounce: { adjustTagInstanceCounts: 0, sortTagList: 0 } };
-    const config = Object.assign(defaultConfig, args.config);
-    return bootOrig({ window, ...args, config });
+    const boot = (args = {}) => {
+        resetJsdom(); 
+        const defaultConfig = { debounce: { adjustTagInstanceCounts: 0, sortTagList: 0 } };
+        const config = Object.assign(defaultConfig, args.config);
+        return bootOrig({ window, ...args, config });
+    };
+
+    return { boot, window, helpers };
 };
+
+const args = setup();
 
 const files = process.argv.slice(2);
 const indent = process.env.INDENT === 'true';
@@ -31,7 +37,7 @@ const runTests = async filePath => {
     await test(filePath, async ({ only, skip, ...t }) => {
         const test = async (...args) => { await t.test(...args); };
         Object.assign(test, { only, skip });
-        await require(path.resolve(filePath))({ test, boot, window, helpers });
+        await require(path.resolve(filePath))({ test, setup, ...args });
     });
 };
 
