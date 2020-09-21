a0f2487b5684a4861f7394c3c3e5b8dbbb5273f7
Matt Riley <mattrileyaus@gmail.com>
2020-09-21 22:17:17 +1000

    remove unused code

--- src/lib/util/index.js
+++ src/lib/util/index.js
@@ -1,7 +1,6 @@
 module.exports = {
     debounce: require('./debounce'),
     mapValues: require('./map-values'),
-    merge: require('./merge'),
     pipe: require('./pipe'),
     splitAt: require('./split-at'),
     upperFirst: require('./upper-first')
