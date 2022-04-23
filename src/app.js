5f6257d0eba652d52e6544ab5334b12c989e2857
Matt Riley <mattrileyaus@gmail.com>
2022-04-23 20:54:01 +1000

    Unspecified changes

--- src/app.js
+++ src/app.js
@@ -5,10 +5,9 @@ const isLocalhost = (/localhost/).test(window.location.host);
 config.gtag.enabled = config.sentry.enabled = !isLocalhost;
 
 const { startup, components, composition } = compose({ config, window });
-const app = { config, ...composition };
-console.log({ app });
-window.app = app;
+window.app = { config, ...composition };
 window.document.title = config.app.name;
+
 const { createRoot } = startup.start();
 const container = document.getElementById('app');
 const root = createRoot(container);
