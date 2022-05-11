1e894b414a8d6b385690376dc217147af1236416
Matt Riley <mattrileyaus@gmail.com>
2022-05-11 20:09:50 +1000

    Unspecified changes

--- src/default-config.js
+++ src/default-config.js
@@ -1,27 +1,25 @@
-// NOTE: Not ".json" to avoid "import assertions".
-
 export default {
-    'gtag': {
-        'trackingId': 'UA-34497639-2',
-        'enabled': false
+    gtag: {
+        trackingId: 'UA-34497639-2',
+        enabled: false
     },
-    'sentry': {
-        'dsn': 'https://63594154fcf34c34966aec13b15e2821@o418187.ingest.sentry.io/5320412',
-        'enabled': false
+    sentry: {
+        dsn: 'https://63594154fcf34c34966aec13b15e2821@o418187.ingest.sentry.io/5320412',
+        enabled: false
     },
-    'app': {
-        'name': 'Agile Avatars',
-        'issues': 'https://github.com/mattriley/agileavatars/issues',
-        'source': 'https://github.com/mattriley/agileavatars'
+    app: {
+        name: 'Agile Avatars',
+        issues: 'https://github.com/mattriley/agileavatars/issues',
+        source: 'https://github.com/mattriley/agileavatars'
     },
-    'author': {
-        'name': 'Matt Riley',
-        'profile': 'https://www.linkedin.com/in/mattrileyau/'
+    author: {
+        name: 'Matt Riley',
+        profile: 'https://www.linkedin.com/in/mattrileyau/'
     },
-    'gravatar': {
-        'domain': 'https://secure.gravatar.com',
-        'size': 600,
-        'fallbacks': [
+    gravatar: {
+        domain: 'https://secure.gravatar.com',
+        size: 600,
+        fallbacks: [
             'robohash',
             'monsterid',
             'wavatar',
@@ -29,97 +27,97 @@ export default {
             'identicon',
             'mp'
         ],
-        'errorMessage': 'An error occurred. Please check your connection and try again.'
+        errorMessage: 'An error occurred. Please check your connection and try again.'
     },
-    'options': {
-        'layout': 'modes | shapes | size | spacing | sort | outline',
-        'modes': [
+    options: {
+        layout: 'modes | shapes | size | spacing | sort | outline',
+        modes: [
             'active',
             'passive'
         ],
-        'shapes': [
+        shapes: [
             'circle',
             'square'
         ],
-        'shapeRadius': {
-            'circle': 50,
-            'square': 0
+        shapeRadius: {
+            circle: 50,
+            square: 0
         },
-        'active': {
-            'min': 0,
-            'max': 999,
-            'step': 1
+        active: {
+            min: 0,
+            max: 999,
+            step: 1
         },
-        'passive': {
-            'min': 0,
-            'max': 999,
-            'step': 1
+        passive: {
+            min: 0,
+            max: 999,
+            step: 1
         },
-        'size': {
-            'min': 100,
-            'max': 600,
-            'step': 10
+        size: {
+            min: 100,
+            max: 600,
+            step: 10
         },
-        'spacing': {
-            'min': 0,
-            'max': 10,
-            'step': 1
+        spacing: {
+            min: 0,
+            max: 10,
+            step: 1
         },
-        'sort': {
-            'orderAdded': 'Order added',
-            'roleThenName': 'Role, then name',
-            'name': 'Name'
+        sort: {
+            orderAdded: 'Order added',
+            roleThenName: 'Role, then name',
+            name: 'Name'
         }
     },
-    'tags': {
-        'layout': 'tagImage | tagName roleName',
-        'imagePadding': 17
+    tags: {
+        layout: 'tagImage | tagName roleName',
+        imagePadding: 17
     },
-    'roles': {
-        'nilRole': {
-            'roleName': '',
-            'color': '#ffffff'
+    roles: {
+        nilRole: {
+            roleName: '',
+            color: '#ffffff'
         },
-        'presetColors': {
-            'BA': '#6688c3',
-            'DEV': '#48a56a',
-            'PO': '#ce4a4a',
-            'QA': '#eaaf41',
-            'TL': '#000000',
-            'XD': '#b25da6'
+        presetColors: {
+            BA: '#6688c3',
+            DEV: '#48a56a',
+            PO: '#ce4a4a',
+            QA: '#eaaf41',
+            TL: '#000000',
+            XD: '#b25da6'
         }
     },
-    'debounce': {
-        'adjustTagInstanceCounts': 100,
-        'sortTagList': 50
+    debounce: {
+        adjustTagInstanceCounts: 100,
+        sortTagList: 50
     },
-    'storage': {
-        'stores': [
+    storage: {
+        stores: [
             'settings',
             'roles',
             'tags',
             'tagInstances'
         ],
-        'defaults': {
-            'settings': {
-                'app': {
-                    'modal': 'welcome',
-                    'nilRoleId': null
+        defaults: {
+            settings: {
+                app: {
+                    modal: 'welcome',
+                    nilRoleId: null
                 },
-                'options': {
-                    'sort': 'orderAdded',
-                    'shape': 'circle',
-                    'active': 1,
-                    'passive': 0,
-                    'size': 170,
-                    'spacing': 4,
-                    'outline': true
+                options: {
+                    sort: 'orderAdded',
+                    shape: 'circle',
+                    active: 1,
+                    passive: 0,
+                    size: 170,
+                    spacing: 4,
+                    outline: true
                 },
-                'gravatar': {
-                    'fallback': 'robohash',
-                    'freetext': '',
-                    'status': 'ready',
-                    'errorMessage': ''
+                gravatar: {
+                    fallback: 'robohash',
+                    freetext: '',
+                    status: 'ready',
+                    errorMessage: ''
                 }
             }
         }
