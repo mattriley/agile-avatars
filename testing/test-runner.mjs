4ad17cd3f4cda886c33bbd3d2e0dc65741508687
Matt Riley <m@ttriley.dev>
2023-06-29 00:15:02 +1000

    Unspecified changes

--- testing/test-runner.mjs
+++ testing/test-runner.mjs
@@ -2,9 +2,8 @@
 
 import test from 'node:test';
 import assert from 'node:assert/strict';
-
-import JSDOM from 'jsdom';
 import process from 'process';
+import JSDOM from 'jsdom';
 import path from 'path';
 
 const testFiles = process.argv.slice(2).map(f => path.resolve(f));
@@ -13,26 +12,20 @@ const { default: composeModules } = await import('../src/compose.js');
 const { default: composeTesting } = await import('./compose.js');
 const { default: defaultTestConfig } = await import('./test-config.js');
 
-const setup = () => {
-    const { window } = new JSDOM.JSDOM('', { url: 'https://localhost/' });
-    const { helpers } = composeTesting({ window }).modules;
+const { window } = new JSDOM.JSDOM('', { url: 'https://localhost/' });
+const { helpers } = composeTesting({ window }).modules;
 
-    const compose = ({ defaults, overrides, config } = {}) => {
-        window.document.getElementsByTagName('html')[0].innerHTML = '';
-        delete window.dataLayer;
-        const options = { defaults, overrides, config: [defaultTestConfig, config] };
-        const composition = composeModules({ window, ...options });
-        composition.modules.startup.start({ composition });
-        return composition;
-    };
-
-    return { compose, window, helpers };
+const compose = ({ defaults, overrides, config } = {}) => {
+    window.document.getElementsByTagName('html')[0].innerHTML = '';
+    delete window.dataLayer;
+    const options = { defaults, overrides, config: [defaultTestConfig, config] };
+    const composition = composeModules({ window, ...options });
+    composition.modules.startup.start({ composition });
+    return composition;
 };
 
-const testTarget = setup();
-
-await Promise.all(testFiles.map(async f => {
+testFiles.forEach(async f => {
     const { default: setup } = await import(f);
     const run = setup({ test, assert });
-    return run(testTarget);
-}));
+    return run({ compose, window, helpers });
+});
